671176dd07ce430c9dbde56ddbd8fbe7f222f00e
javiglejun <javiglejun@gmail.com>
2026-07-02 12:38:44 +0200

    Update FollowUp_Enagas.py

--- FollowUp_Enagas.py
+++ FollowUp_Enagas.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
+import pandas as pd
 import requests
 import os
 
@@ -8,7 +9,7 @@ hoy = datetime.now(ZoneInfo("Europe/Madrid")).date()
 fecha = hoy - timedelta(days=1)
 
 fecha_url = fecha.strftime("%d/%m/%Y")
-fecha_nombre = fecha.strftime("%Y%m%d")
+fecha_csv = fecha.strftime("%Y-%m-%d")
 
 url = (
     "https://www.enagas.es/content/enagas/es/gestion-tecnica-sistema/"
@@ -17,15 +18,66 @@ url = (
     f"dailysystemtracking.dailysystemtrackingdto.xls?date={fecha_url}"
 )
 
-os.makedirs("data", exist_ok=True)
-
-nombre_fichero = f"data/ParPreDia{fecha_nombre}.xls"
-
+# Descargar excel temporal
 r = requests.get(url, timeout=60)
+r.raise_for_status()
 
-if r.status_code == 200:
-    with open(nombre_fichero, "wb") as f:
-        f.write(r.content)
-    print(f"Descargado: {nombre_fichero}")
+excel_temp = "temporal.xlsx"
+
+with open(excel_temp, "wb") as f:
+    f.write(r.content)
+
+# Leer hoja
+df = pd.read_excel(excel_temp, header=None)
+
+# Convertir toda la hoja a lista
+valores = df.fillna("").values.tolist()
+
+demanda_nacional = None
+convencional = None
+sector_electrico = None
+
+for i, fila in enumerate(valores):
+
+    if "Demanda Nacional" in map(str, fila):
+        demanda_nacional = float(valores[i+1][1])
+
+    if "Convencional" in map(str, fila):
+        convencional = float(valores[i+1][1])
+
+    if "Sector Eléctrico" in map(str, fila):
+        sector_electrico = float(valores[i+1][1])
+
+# Crear registro
+nuevo = pd.DataFrame([{
+    "Fecha": fecha_csv,
+    "Demanda Nacional": demanda_nacional,
+    "Convencional": convencional,
+    "Sector Eléctrico": sector_electrico
+}])
+
+csv_file = "historico_demanda.csv"
+
+if os.path.exists(csv_file):
+    historico = pd.read_csv(csv_file)
+
+    historico = historico[
+        historico["Fecha"] != fecha_csv
+    ]
+
+    historico = pd.concat(
+        [historico, nuevo],
+        ignore_index=True
+    )
 else:
-    raise Exception(f"Error {r.status_code}")
+    historico = nuevo
+
+historico = historico.sort_values("Fecha")
+
+historico.to_csv(
+    csv_file,
+    index=False,
+    encoding="utf-8-sig"
+)
+
+print(historico.tail())
